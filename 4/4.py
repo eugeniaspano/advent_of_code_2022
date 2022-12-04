8e1973ff8e4d5c6b3e7cfb0f0a7873aa39a973f3
Eugenia Spano <eugenia.spano@findwise.com>
2022-12-04 10:25:58 +0100

    day 4

---
+++ 4/4.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    file = open("input.txt")
+    test_file = open("test_input.txt")
+    lines = file.readlines()
+
+    overlap = 0
+    for pairs in lines:
+        list_pairs = pairs.split(',')
+
+        first = [x for x in range(int(list_pairs[0].split('-')[0]), int(list_pairs[0].split('-')[1].strip())+1)]
+        second = [x for x in range(int(list_pairs[1].split('-')[0]), int(list_pairs[1].split('-')[1].strip())+1)]
+
+        if all(s in first for s in second) or all(f in second for f in first):
+            overlap += 1
+
+    print(overlap)
