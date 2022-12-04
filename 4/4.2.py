e2623885a35f44b17374f38227a201da31462410
Eugenia Spano <eugenia.spano@findwise.com>
2022-12-04 10:38:04 +0100

    added comment for more clarity in the one-liner

--- 4/4.2.py
+++ 4/4.2.py
@@ -5,10 +5,11 @@ if __name__ == "__main__":
 
     overlap = 0
     for pairs in lines:
-        list_pairs = pairs.split(',')
+        list_pairs = pairs.strip().split(',')
 
-        first = [x for x in range(int(list_pairs[0].split('-')[0]), int(list_pairs[0].split('-')[1].strip()) + 1)]
-        second = [x for x in range(int(list_pairs[1].split('-')[0]), int(list_pairs[1].split('-')[1].strip()) + 1)]
+        # list_pairs = ['2-4', '3-7'] --> first = [2, 3, 4] second = [3, 4, 5, 6, 7]
+        first = [x for x in range(int(list_pairs[0].split('-')[0]), int(list_pairs[0].split('-')[1]) + 1)]
+        second = [x for x in range(int(list_pairs[1].split('-')[0]), int(list_pairs[1].split('-')[1]) + 1)]
 
         if any(s in first for s in second) or any(f in second for f in first):
             overlap += 1
