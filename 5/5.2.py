aaf0dfa7420cdaa7e778cd74b7cb589175addb70
Eugenia Spano <eugenia.spano@findwise.com>
2022-12-05 16:46:13 +0100

    cleaned code and created functions

--- 5/5.2.py
+++ 5/5.2.py
@@ -2,49 +2,57 @@
 import re
 from pprint import pprint
 
-if __name__ == "__main__":
-    file = open("input.txt")
-    test_file = open("test_input.txt")
-    lines = file.readlines()
 
-    for i, line in enumerate(lines):
-        if line == '\n':
-            setup = lines[:i]
-            moves = lines[i+1:]
-
+def parse_setup_into_stacks(setup):
     stacks = {}
-    n_stacks = int(setup[-1].split()[-1])
 
     for setup_line in setup[:-1]:
-        crate_positions = [match.start() for match in re.finditer('\[', setup_line)]
+        crate_positions = [match.start() for match in re.finditer(r'\[', setup_line)]
         for crate_pos in crate_positions:
-            stack_num = int(crate_pos/4 + 1)
-            crate = setup_line[crate_pos+1]
+            stack_num = int(crate_pos / 4 + 1)
+            crate = setup_line[crate_pos + 1]
 
             if stack_num not in stacks:
                 stacks[stack_num] = list()
             stacks[stack_num].append(crate)
-
     for stack_num, stack in stacks.items():
         stacks[stack_num] = list(reversed(stack))
 
-    # pprint(stacks)
+    return stacks
 
+
+def move_crates(moves, stacks):
     for move in moves:
-        (quantity, from_crate, to_crate) = [match.group() for match in re.finditer('\d+', move)]
+        (quantity, from_crate, to_crate) = [match.group() for match in re.finditer(r'\d+', move)]
         quantity = int(quantity)
         from_crate = int(from_crate)
         to_crate = int(to_crate)
 
         stacks[to_crate].extend(list(stacks[from_crate][-quantity:]))
         stacks[from_crate] = stacks[from_crate][:-quantity]
-
         # pprint(stacks)
 
-    # pprint(stacks)
+    return stacks
+
+
+def main():
+    file = open("input.txt")
+    test_file = open("test_input.txt")
+    content = file.read()
+
+    setup, moves = [section.splitlines() for section in content.split('\n\n')]
+    stacks = parse_setup_into_stacks(setup)
+    rearranged_stacks = move_crates(moves, stacks)
 
     solution = ''
-
-    for n in range(1, n_stacks+1):
-        solution += stacks[n][-1]
+    for n in range(1, len(rearranged_stacks) + 1):
+        solution += rearranged_stacks[n][-1]
     print(solution)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
