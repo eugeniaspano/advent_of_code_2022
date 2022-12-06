cefd6431c44fa8f5ebffaaa89b73966d16f39329
Eugenia Spano <eugenia.spano@findwise.com>
2022-12-06 07:33:35 +0100

    day 6

---
+++ 6/6.2.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+    file = open("input.txt")
+    test_file = open("test_input.txt")
+    input = file.read()
+
+    list_input = [x for x in input.strip()]
+
+    sequence_length = 14
+
+    for i in range(len(input)-sequence_length):
+        sequence = list_input[i: i+sequence_length]
+        if len(sequence) == len(set(sequence)):
+            print(i+sequence_length, sequence)
+            break
+
+
+
